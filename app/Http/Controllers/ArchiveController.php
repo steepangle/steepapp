4c8ba817dd4dd220e7d51443a49355ee9c363c2a
flan6002 <f.and@gmx.at>
2026-01-21 12:47:46 +0100

    controller for archive updated + login no email

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -14,23 +14,23 @@ class ArchiveController extends Controller
 
         return response()->file(storage_path('app/' . $archive->file_path));
     }
-}
 
-public function index()
-{
-    $user = Auth::user(); // get currently logged-in user
+    public function index()
+    {
+        $user = Auth::user(); // get currently logged-in user
 
-    // Start query: public archives visible to everyone
-    $query = ArchiveItem::where('access_level', 'public');
+        // Start query: public archives visible to everyone
+        $query = ArchiveItem::where('access_level', 'public');
 
-    // If a user is logged in, also include private archives for their groups
-    if ($user) {
-        $query->orWhereHas('userGroups', function($q) use ($user) {
-            $q->whereIn('user_groups.id', $user->userGroups->pluck('id'));
-        });
+        // If a user is logged in, also include private archives for their groups
+        if ($user) {
+            $query->orWhereHas('userGroups', function($q) use ($user) {
+                $q->whereIn('user_groups.id', $user->userGroups->pluck('id'));
+            });
+        }
+
+        $archives = $query->get(); // execute query
+
+        return view('archives.index', compact('archives')); // send to view
     }
-
-    $archives = $query->get(); // execute query
-
-    return view('archives.index', compact('archives')); // send to view
 }
