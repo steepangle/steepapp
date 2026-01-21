05015f8abb96e2fe26a1937cb8e49e0f4e9a2f87
flan6002 <f.and@gmx.at>
2026-01-21 13:34:48 +0100

    new archiveController

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -3,34 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\ArchiveItem;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class ArchiveController extends Controller
 {
-    public function show(ArchiveItem $archive)
-    {
-        $this->authorize('view', $archive);
-
-        return response()->file(storage_path('app/' . $archive->file_path));
-    }
-
     public function index()
     {
-        $user = Auth::user(); // get currently logged-in user
+        $user = Auth::user(); // currently logged-in user
+        $userGroupIds = $user ? $user->userGroups->pluck('id')->toArray() : [];
 
-        // Start query: public archives visible to everyone
-        $query = ArchiveItem::where('access_level', 'public');
+        // Fetch archives:
+        // - public archives
+        // - or archives where user's groups have access
+        $archives = ArchiveItem::where('access_level', 'public')
+            ->orWhereExists(function ($query) use ($userGroupIds) {
+                $query->selectRaw('1')
+                      ->from('archive_item_user_groups')
+                      ->whereColumn('archive_item_user_groups.archive_item_id', 'archive_items.id')
+                      ->whereIn('archive_item_user_groups.user_group_id', $userGroupIds);
+            })
+            ->get();
 
-        // If a user is logged in, also include private archives for their groups
-        if ($user) {
-            $query->orWhereHas('userGroups', function($q) use ($user) {
-                $q->whereIn('user_groups.id', $user->userGroups->pluck('id'));
-            });
-        }
-
-        $archives = $query->get(); // execute query
-
-        return view('archives.index', compact('archives')); // send to view
+        return view('archives.index', compact('archives'));
     }
 }
+
